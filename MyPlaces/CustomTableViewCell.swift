1d343d431f2620869dd02eaaad0ea0540dcc00ad
Fortovaya <wilov@yandex.ru>
2024-04-27 22:42:21 +0300

    Custom Table View Cell (Fix Git)

--- MyPlaces/CustomTableViewCell.swift
+++ MyPlaces/CustomTableViewCell.swift
@@ -9,11 +9,9 @@ import UIKit
 
 class CustomTableViewCell: UITableViewCell {
 
-    
     @IBOutlet weak var imageOfPlace: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var typeLabel: UILabel!
-    
     
 }
