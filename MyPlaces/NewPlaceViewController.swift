21460b6f1df15825b3b092125e7555dcd949caa5
Fortovaya <wilov@yandex.ru>
2024-04-29 15:35:14 +0300

    Image Picker Controller: adding photos via camera / photo album, adding shooting rights to info

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -19,6 +19,28 @@ class NewPlaceViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if indexPath.row == 0 {
+            let actionSheet = UIAlertController(title: nil,
+                                                message: nil,
+                                                preferredStyle: .actionSheet)
+            
+            let camera = UIAlertAction(title: "Camera",
+                                       style: .default) {_ in
+                self.chooseImagePicker(source: .camera)
+            }
+            
+            let photo = UIAlertAction(title: "Photo",
+                                      style: .default) {_ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel",
+                                       style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
             
         } else {
             view.endEditing(true)
@@ -39,4 +61,21 @@ extension NewPlaceViewController: UITextFieldDelegate {
         return true
     }
     
+}
+
+// MARK: - Work with image
+
+extension NewPlaceViewController {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType){
+        
+        if UIImagePickerController.isSourceTypeAvailable(source){
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true // масштабирование изображения
+            imagePicker.sourceType = source
+            
+            present(imagePicker, animated: true)
+        }
+        
+    }
 }
